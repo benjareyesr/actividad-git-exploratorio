b566a30488e3e531be60ec64684149de5c0b377b
Rafasantibanez <rusantibanez@uc.cl>
2026-08-18 16:22:16 -0400

    cuando gana jugador 1 normal

--- actividad.py
+++ actividad.py
@@ -1,20 +1,11 @@
-def sumar(x,y):
-    return x + y
+j1=input("jugador 1 elije una acción:")
+j2=input("jugador 2elije una acción:")
 
-def restar(x,y):
-    return x - y
-
-def multiplicar(x,y):
-    return x * y
-
-def dividir(x,y):
-    return x/y
-
-def hola():
-    return "hola"
-
-def chao():
-    return "chao"
-
-def test():
-    return "prueba"
+if j1==j2:
+    print("empate")
+elif j1=="piedra" and j2=="tijera":
+    print("jugador 1 gana")
+elif j1=="tijera" and j2=="papel":
+    print("jugador 1 gana")
+elif j1=="papel" and j2=="piedra":
+    print("jugador 1 gana")
